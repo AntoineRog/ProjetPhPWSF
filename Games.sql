f66eb0090d85632492a1bc6e3392837cb13b283f
Yulay77 <91956920+Yulay77@users.noreply.github.com>
2022-05-24 21:57:41 +0200

    Update Games.sql

--- Games.sql
+++ Games.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: May 16, 2022 at 04:55 PM
+-- Generation Time: May 24, 2022 at 06:59 PM
 -- Server version: 5.7.24
 -- PHP Version: 7.4.1
 
@@ -21,6 +21,28 @@ SET time_zone = "+00:00";
 --
 -- Database: `games`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `editor`
+--
+
+CREATE TABLE `editor` (
+  `id` int(11) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `country` varchar(20) NOT NULL,
+  `city` varchar(20) NOT NULL,
+  `description` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `editor`
+--
+
+INSERT INTO `editor` (`id`, `name`, `country`, `city`, `description`) VALUES
+(1, 'Ubisoft', 'France', 'Paris', 'Ubisoft c\'est vraiment super !'),
+(2, 'Gorilla', 'Allemagne', 'Berlin', 'Créateurs du jeu archero ce sont des génies.');
 
 -- --------------------------------------------------------
 
@@ -44,11 +66,17 @@ CREATE TABLE `games` (
 
 INSERT INTO `games` (`id`, `name`, `description`, `price`, `device`, `genre`, `editor`) VALUES
 (1, 'Trackmania', 'Trackmania est un jeu de voiture qui a des bases très classique d\'un jeu de course mais qui possède des mécaniques de jeux inattendues. Vous ne pourrez jamais atteindre le temps le plus optimisé ce qui rend la compétition sur ce jeu intéressante. De plus, elle peut être diffusée à tout public car les bases du jeu sont simple à comprendre. Il y a un départ, une arrivée et des checkpoints.', 36, 'Desktop', 'Racing', 'Ubisoft'),
-(2, 'Archero', 'Archero est un jeu mobile qui se joue avec la vue du dessus de votre héros. Vous aurez de multiples châteaux à parcourir par étages. Votre but, finir tous les châteaux rempli de monstres les plus forts. Améliorez votre équipement afin de les vaincre.', 1, 'Phone', 'floors levels', 'gorilla');
+(2, 'Archero', 'Archero est un jeu mobile qui se joue avec la vue du dessus de votre héros. Vous aurez de multiples châteaux à parcourir par étages. Votre but, finir tous les châteaux rempli de monstres les plus forts. Améliorez votre équipement afin de les vaincre.', 1, 'Phone', 'floors levels', 'Gorilla');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `editor`
+--
+ALTER TABLE `editor`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `games`
@@ -59,6 +87,12 @@ ALTER TABLE `games`
 --
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `editor`
+--
+ALTER TABLE `editor`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `games`
